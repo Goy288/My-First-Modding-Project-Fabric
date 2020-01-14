d985876742b29737756af69e4c88f2472c669bf3
goy288 <sherwood8199@students.cptc.edu>
2020-01-14 15:54:00 -0800

    Added various personal changes

---
+++ src/main/java/net/goy288/mymod/MyMod.java
@@ -0,0 +1,12 @@
+package net.goy288.mymod;
+
+import net.fabric.api.ModInitializer;
+
+public class CoolMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+    
+}
