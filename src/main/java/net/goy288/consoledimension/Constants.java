cc86e678a2a9087e9c84346cc54376f77e457491
Cameron Sherwood <cameronsherwood1@gmail.com>
2020-01-22 15:43:46 -0800

    Currently rewriting the mod. Changed the mod ID.

---
+++ src/main/java/net/goy288/consoledimension/Constants.java
@@ -0,0 +1,5 @@
+package net.goy288.consoledimension;
+
+public class Constants {
+    public static final String MOD_ID = "consoledimension";
+}
---
+++ src/main/java/net/goy288/consoledimension/ModInit.java
@@ -0,0 +1,20 @@
+package net.goy288.consoledimension;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityCategory;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SpawnEggItem;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome.SpawnEntry;
+
+public class ModInit implements ModInitializer {
+    
+    
+    @Override
+	public void onInitialize() {
+    }
+}
